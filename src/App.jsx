4916e792f0c4770f15007ceeadb64b51eb04c5b5
abanoub-refaat <abanoubref3at18@gmail.com>
2024-12-09 18:03:47 +0200

    fix typo and add login and defualt route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
+import Login from "./components/Login";
+
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
-  
-
 }
 
 export default App;
