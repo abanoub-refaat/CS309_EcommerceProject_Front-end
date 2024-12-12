1f32b100ef8bfb373dcd045a9aeecd8a502afe61
abanoub-refaat <abanoubref3at18@gmail.com>
2024-12-12 21:39:28 +0200

    fix style typo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
-import Error from "./components/error";
+import Error from "./components/Error";
 
 function App() {
   return (
