d646d99bab765fe186ede87c118c79b7e0289eb9
abanoub-refaat <abanoubref3at18@gmail.com>
2024-12-18 22:15:04 +0200

    fix typos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Error from "./components/Error";
-import Forget from "./components/Forget";
 import AboutUs from "./components/AboutUs";
 function App() {
   return (
@@ -12,8 +11,7 @@ function App() {
         <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/Forget" element={<Forget />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
+        <Route path="/about-us" element={<AboutUs />} />
         <Route path="/*" element={<Error />} />
       </Routes>
     </Router>
