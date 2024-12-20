b97625177f29a6719390a0c5dbb73479e6c5cf49
abanoub-refaat <abanoubref3at18@gmail.com>
2024-12-21 01:02:26 +0200

    fix some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
@@ -10,17 +11,18 @@ import ProductDetailsPage from "./components/ProductDetailsPage";
 import AboutUs from "./components/AboutUs";
 import UserProfile from "./components/UserProfile";
 import Wishlist from "./components/Wishlist";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Router>
-      <Navbar />
+        <Navbar />
         <Routes>
-          <Route path="/userProfile" element={<UserProfile />}/>
+          <Route path="/userProfile" element={<UserProfile />} />
           <Route path="/products" element={<Products />} />
           <Route path="/product/:id" element={<ProductDetailsPage />} />
-          <Route path="/wishlist" element={<Wishlist/>} />
+          <Route path="/wishlist" element={<Wishlist />} />
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
@@ -28,6 +30,7 @@ function App() {
           <Route path="/about" element={<AboutUs />} />
           <Route path="/*" element={<Error />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );
--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,78 +1,72 @@
+import React from "react";
 import "./AboutUs.css";
 import "./Member";
 import Member from "./Member";
 
 const AboutUs = () => {
   const Team = [
-    
     {
-      id :1,
-      name : "Abanoub Refaat" ,
-      img : "./assets/bob.png",
-      github : "https://github.com/abanoub-refaat"
-    }
-    ,
+      id: 1,
+      name: "Abanoub Refaat",
+      img: "./assets/bob.png",
+      github: "https://github.com/abanoub-refaat",
+    },
     {
-      id :2,
-      name : "Antonios Samy" ,
-      img : "./assets/tony.png",
-      github : "https://github.com/tonyBuffon"
-    }
-    ,
+      id: 2,
+      name: "Antonios Samy",
+      img: "./assets/tony.png",
+      github: "https://github.com/tonyBuffon",
+    },
     {
-      id :3,
-      name : "Fady Monir", 
-      img : "./assets/foda.png",
-      github : "https://github.com/fadymonir10"
-    }
-  
- 
-    ,
+      id: 3,
+      name: "Fady Monir",
+      img: "./assets/foda.png",
+      github: "https://github.com/fadymonir10",
+    },
+
     {
-      id :4,
-      name : "Menna Ayman" ,
-      img : "./assets/manon.png",
-      github : "https://github.com/ME-nna"
-    }
-    ,
+      id: 4,
+      name: "Menna Ayman",
+      img: "./assets/manon.png",
+      github: "https://github.com/ME-nna",
+    },
     {
-      id :5,
-      name : "Ebram Wael", 
-      img : "./assets/bero.png",
-      github : "https://github.com/Ebram-Wael"
-    }
-    ,
+      id: 5,
+      name: "Ebram Wael",
+      img: "./assets/bero.png",
+      github: "https://github.com/Ebram-Wael",
+    },
     {
-      id :6,
-      name : "Esraa Hassan", 
-      img : "./assets/asora.png",
-      github : "https://github.com/xxesraahassan"
-    }
-    ,
+      id: 6,
+      name: "Esraa Hassan",
+      img: "./assets/asora.png",
+      github: "https://github.com/xxesraahassan",
+    },
     {
-      id :7,
-      name : "Youssef Emad" ,
-      img : "./assets/joe.png",
-      github : "https://github.com/youssef-emad347"
-    }
-  
+      id: 7,
+      name: "Youssef Emad",
+      img: "./assets/joe.png",
+      github: "https://github.com/youssef-emad347",
+    },
   ];
   return (
-  <div className="about-container">
+    <div className="about-container">
       <h1>Our Team</h1>
       <ul className="member-list">
-      {Team.map((member) => (
-       <li key = {member.id} className="member"> <Member 
-        key = {member.id}
-        name = {member.name}
-        img = {member.img}
-        github = {member.github} /> </li>
-      ))}
+        {Team.map((member) => (
+          <li key={member.id} className="member">
+            {" "}
+            <Member
+              key={member.id}
+              name={member.name}
+              img={member.img}
+              github={member.github}
+            />{" "}
+          </li>
+        ))}
       </ul>
-
     </div>
-  )
-    
-  };
+  );
+};
 
 export default AboutUs;
