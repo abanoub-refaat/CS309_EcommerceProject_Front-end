8761b0b68ff5daddb33dcb041908a71f7fbd1397
Youssef Emad <youssefemad347@gmail.com>
2024-12-20 21:50:11 +0200

    add Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./Hero.css";
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <h1>Discover Your Next Favorite Thing</h1>
+        <p>
+          Shop the latest trends, unbeatable deals, and exclusive collections
+          <br />all in one place.
+        </p>
+        <div className="go-to-button">
+        <Link to={"/products"} className="hero-button">
+        Explore Products
+        </Link>
+      </div>
+      </div>
+      <div className="hero-image">
+        <img src="./assets/hero.svg" alt="Hero-image" />
+      </div>
+    </div>
+  );
+}
