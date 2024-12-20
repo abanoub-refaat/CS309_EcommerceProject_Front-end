1d3ae3f2791833f87a16e3dc698ba20e28a58069
xxesraahassan <xxesraahassan@gmail.com>
2024-12-20 20:08:29 +0200

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,12 @@
-import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-
+import "./footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-container">
         <div className="footer-logo">
-          <h2>OurSite</h2>
+          <h2>DealZone</h2>
         </div>
-
         <div className="footer-links">
           <div className="footer-column">
             <h3>Information</h3>
@@ -19,10 +15,9 @@ const Footer = () => {
               <li><Link to="/login">Login</Link></li>
               <li><Link to="/cart">My Cart</Link></li>
               <li><Link to="/wishlist">My Wishlist</Link></li>
-              
             </ul>
           </div>
-          <div className="footer-column">
+          <div className="footer-column2">
             <h3>Service</h3>
             <ul>
               <li><Link to="/about">About Us</Link></li>
@@ -31,20 +26,17 @@ const Footer = () => {
           </div>
         </div>
 
-      <div className="footer-bottom">
-        <p>©2023 OurSite All Rights are reserved</p>
-        <div className="footer-payment">
-          <img src="visa.png" alt="Visa" />
-          <img src="mastercard.png" alt="Mastercard" />
-          <img src="gpay.png" alt="Google Pay" />
-          <img src="amex.png" alt="American Express" />
-          <img src="paypal.png" alt="PayPal" />
-        </div>
-        <div className="footer-social">
-        <h1>Follow us on Social Media</h1>
-        <FontAwesomeIcon icon="fa-brands fa-facebook" />
-        <FontAwesomeIcon icon="fa-brands fa-twitter" /> 
-        <FontAwesomeIcon icon="fa-brands fa-instagram" />
+        <div className="footer-bottom">
+          <p>©2024 OurSite All Rights are reserved</p>
+          <div className="footer-payment">
+          
+          </div>
+          <div className="footer-social">
+            <h1>Follow us on Social Media</h1>
+            <a href="https://www.facebook.com/yourpage" target="_blank"><i class="fab fa-facebook"></i></a>
+            <a href="https://twitter.com/yourprofile" target="_blank"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/yourprofile" target="_blank"><i class="fab fa-instagram"></i></a>       
+          </div>
         </div>
       </div>
     </footer>
@@ -52,3 +44,6 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
+
