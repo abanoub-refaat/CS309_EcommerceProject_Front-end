74a5afc4ba6a4a5829bb6778e1537b90040afa7e
Ebram-Wael <202227393@std.sci.cu.edu.eg>
2024-12-20 15:07:06 +0200

    add intial products component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Error from "./components/Error";
 import Forget from "./components/Forget";
+import Products from "./components/Products";
+
 function App() {
   return (
     <Router>
       <Routes>
+      <Route path="/products" element={<Products />} />
         <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
